c9dacf30c44203e577be4298fc6d4610d33211b7
ismaelarias <ismaeljarias@gmail.com>
2026-01-17 13:28:16 -0600

    custom js to hide and show input fields

--- assets/contact-form-custom.js
+++ assets/contact-form-custom.js
@@ -1,1 +1,28 @@
-//logic for our custom contact form
+document.addEventListener("DOMContentLoaded", function() {
+  const businessAccount = document.getElementById("businessAccount");
+  const companyInfo = document.getElementById("companyInfo");
+  const contactReason = document.getElementById("contactReason");
+  const productNameField = document.getElementById("productNameField");
+  const orderNumberField = document.getElementById("orderNumberField");
+
+  // Event listeners
+  businessAccount.addEventListener("change", function() {
+    if (businessAccount.checked) {
+      companyInfo.classList.remove("hidden");
+    } else {
+      companyInfo.classList.add("hidden");
+    }
+  });
+
+  contactReason.addEventListener("change", function() {
+    productNameField.classList.add("hidden");
+    orderNumberField.classList.add("hidden");
+
+    if (contactReason.value === "product") {
+      productNameField.classList.remove("hidden");
+    } else if (contactReason.value === "order") {
+      orderNumberField.classList.remove("hidden");
+    }
+  });
+
+});
