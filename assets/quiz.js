8f4ab361894404263680d44f3ddad0c317e08d4a
ismaelarias <ismaeljarias@gmail.com>
2026-01-17 15:20:51 -0600

    store and update answers in object

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,13 +1,16 @@
 const finish_btn = document.getElementById("finish-button");
+finish_btn.disabled = true;
 
 const quiz_form = document.querySelector("#quizForm");
 const answers = {};
 
+
 quiz_form.addEventListener("change", updateAnswers);
 function updateAnswers(event) {
-  //code to store answers
+  let input = event.target;
+  answers[input.name] = input.value;
 
-
+  finish_btn.disabled = !(answers.product_type && answers.bike_type && answers.color);
 
   console.log("answers: ", answers);
 }
