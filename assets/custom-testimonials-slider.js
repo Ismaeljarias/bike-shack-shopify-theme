0fe18e7f3557cf6a79f37866e14661ed51e536a5
ismaelarias <ismaeljarias@gmail.com>
2026-01-20 18:40:26 -0600

    blocks rendered dynamically, and slider initialize

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -7,3 +7,12 @@
 // infinite loop
 // the slides or cells should be aligned to the left side on initialization.
 // pauseAutoPlayOnHover: false
+const slideshow_container = document.querySelector(".testimonial-slider");
+const slider = new Flickity(slideshow_container, {
+  pageDots: false,
+	wrapAround: true,
+	autoPlay: false,
+  cellAlign: 'left',
+  pauseAutoPlayOnHover: false,
+	// fullscreen: true,
+});
