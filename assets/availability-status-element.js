9c717f454c1d5871fbc08b57af7812c28a7930bf
ismaelarias <ismaeljarias@gmail.com>
2026-01-28 07:51:37 -0600

    handle variant change and initialize

--- assets/availability-status-element.js
+++ assets/availability-status-element.js
@@ -4,7 +4,7 @@ class AvailabilityStatus extends HTMLElement {
 
     //Initial values
     this.selectedLocation = "New York";
-    this.selectedVariantColor =  "Navy Blue"
+    this.selectedVariantColor =  this.dataset.initialvariant;
     this.selectedVariantStockData = null;
 
     //UI Elements
@@ -27,11 +27,15 @@ class AvailabilityStatus extends HTMLElement {
     );
 
     //initialize! 
+    this.updateAvailability();
     
   }
 
   handleVariantChange(event) {
     /* update  this.selectedVariantColor based on the Shopify variant change-event */
+    const newColor = event.detail.newVariant.option1;
+    console.log(event)
+    this.selectedVariantColor = newColor;
    
     this.updateAvailability();
   }
